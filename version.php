dabf379ca49142913a0b9672725951f94d003015
Petr Skoda <petr@mutms.org>
2026-03-26 10:38:12 +0100

    Back to dev

--- version.php
+++ version.php
@@ -31,7 +31,7 @@ $plugin->component = 'tool_muhome';
 $plugin->version = 2026032650;
 $plugin->requires = 2024100700;
 $plugin->supported = [500, 501];
-$plugin->release = 'v5.0.6.03';
+$plugin->release = 'v5.0.6.03+';
 
 $plugin->dependencies = [
     'tool_mulib' => 2026032650,
