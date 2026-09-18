5bbcc0bdd358d197dff5c10c962131ec3237cb9a
Petr Skoda <petr@mutms.org>
2026-09-18 23:40:46 +0200

    Separate version from stable

--- version.php
+++ version.php
@@ -28,11 +28,11 @@ defined('MOODLE_INTERNAL') || die();
 
 /** @var stdClass $plugin */
 $plugin->component = 'tool_muhome';
-$plugin->version = 2026091953;
+$plugin->version = 2026092353;
 $plugin->requires = 2024100700;
 $plugin->supported = [503, 503];
 $plugin->release = 'v5.3.0.00';
 
 $plugin->dependencies = [
-    'tool_mulib' => 2026091953,
+    'tool_mulib' => 2026092353,
 ];
